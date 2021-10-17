158a6fdaf5b9d289af681e73387cd4a0b4a16989
sohaghosen <sohag.hosen@outlook.com>
2021-10-18 00:22:12 +0600

    hero section responsiv

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -2,15 +2,15 @@ import React from "react";
 import banner from "../../assets/images/banner.png";
 function HeroSection() {
   return (
-    <div class=" container mx-auto mt-14">
-      <div class="flex">
-        <main class="w-1/2  flex justify-center items-center">
-          <div class="sm:text-center lg:text-left">
+    <div class="container mx-auto mt-14">
+      <div class="md:flex gap-x-2">
+        <main class="md:w-1/2  flex justify-center items-center">
+          <div class="text-center lg:text-left">
             <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span class="block xl:inline">We Only Give Best Care To </span>
               <span class="block text-indigo-600 xl:inline">Your Eyes</span>
             </h1>
-            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            <p class="mt-3  text-gray-500 sm:mt-5  sm:max-w-xl sm:mx-auto md:mt-5  lg:mx-0">
               Eyes are organs of the visual system. They provide animals with
               vision, the ability to receive and process visual detail, as well
               as enabling several photo response functions that are independent
@@ -21,10 +21,10 @@ function HeroSection() {
             </button>
           </div>
         </main>
-        <div className="w-1/2 flex justify-center">
+        <div className="md:w-1/2 flex justify-center mt-8 md:mt-0">
           <img
-            style={{ height: "500px" }}
-            class="h-32 w-auto object-cover sm:h-72 md:h-96 lg:h-96"
+            style={{ maxHeight: "500px" }}
+            class=" w-auto  md:h-96 lg:w-auto lg:h-full"
             src={banner}
             alt=""
           />
